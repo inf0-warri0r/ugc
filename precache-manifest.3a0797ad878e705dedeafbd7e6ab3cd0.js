62a5d8334e7d60fcf36e5d8222e1218a00dfd3f4
tharindra galahena <tcg.galahena@gmail.com>
2022-09-14 13:26:08 +0530

    Updates

---
+++ precache-manifest.3a0797ad878e705dedeafbd7e6ab3cd0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "86e97e3733070bbad86e",
+    "url": "/ugc/static/css/main.a18c1c0e.chunk.css"
+  },
+  {
+    "revision": "86e97e3733070bbad86e",
+    "url": "/ugc/static/js/main.86e97e37.chunk.js"
+  },
+  {
+    "revision": "2d0620cea52edf1fd600",
+    "url": "/ugc/static/js/1.2d0620ce.chunk.js"
+  },
+  {
+    "revision": "73ecc2b9f835e9340db7",
+    "url": "/ugc/static/css/2.e9effa32.chunk.css"
+  },
+  {
+    "revision": "73ecc2b9f835e9340db7",
+    "url": "/ugc/static/js/2.73ecc2b9.chunk.js"
+  },
+  {
+    "revision": "7bbe8ac9b35e2756d7cb",
+    "url": "/ugc/static/js/runtime~main.7bbe8ac9.js"
+  },
+  {
+    "revision": "a41799cacf5963c04fcee845d96578d3",
+    "url": "/ugc/static/media/UCSClogo.a41799ca.png"
+  },
+  {
+    "revision": "673c781076abe1b057bf963f2438ae40",
+    "url": "/ugc/index.html"
+  }
+];
